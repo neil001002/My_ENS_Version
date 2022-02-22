a5549e04e88e6b011896fc231646793e9d4dd1a7
Sunil Kumar <71462446+neil001002@users.noreply.github.com>
2022-02-22 11:10:16 +0530

    final commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,21 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("ninja");
+    const domainContract = await domainContractFactory.deploy("dev");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
   
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-      let txn = await domainContract.register("banana",  {value: hre.ethers.utils.parseEther('0.1')});
+      let txn = await domainContract.register("HelloWorld",  {value: hre.ethers.utils.parseEther('0.1')});
       await txn.wait();
-    console.log("Minted domain banana.ninja");
+    console.log("Minted domain HelloWorld.dev");
   
-    txn = await domainContract.setRecord("banana", "Am I a banana or a ninja??");
+    txn = await domainContract.setRecord("HelloWorld", "Hello world from sunil??");
     await txn.wait();
-    console.log("Set record for banana.ninja");
+    console.log("Set record for HelloWorld.dev");
   
-    const address = await domainContract.getAddress("banana");
-    console.log("Owner of domain banana:", address);
+    const address = await domainContract.getAddress("HelloWorld");
+    console.log("Owner of domain HelloWorld:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
